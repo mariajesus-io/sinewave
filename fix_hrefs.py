41fc42449ad7cc3845f9b43fcf709ffbfff9d2ca
mariajesus-io <maria.erices08@inacapmail.cl>
2026-09-06 11:59:25 -0300

    archivos de gemini borrados

--- fix_hrefs.py
+++
@@ -1,9 +0,0 @@
-with open('/home/maria/sinewave/tienda/templates/index.html', 'r') as f:
-    content = f.read()
-
-content = content.replace('href="#mas-visto"', 'href="#categorias-grid"')
-
-with open('/home/maria/sinewave/tienda/templates/index.html', 'w') as f:
-    f.write(content)
-
-print("Hrefs fixed")
